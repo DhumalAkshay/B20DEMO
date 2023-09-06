dd9d3e267b0c006795b91385894724de76de3b9d
dhumalakshay <dhumal.akshay659@gmail.com>
2023-09-06 13:10:48 +0530

    added add method with a new calculator class

--- DemoSolution/First/Program.cs
+++ DemoSolution/First/Program.cs
@@ -1,7 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using First;
 
-Console.WriteLine("Thank You");
+Console.WriteLine("Welcome to Calculator App");
+
+Console.WriteLine("Please Enter the first Number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please Enter the Second Number");
+int b = int.Parse(Console.ReadLine());
+
+
+
+
+Calculator c = new Calculator();
+c.Add(a, b);
 
 
 Console.ReadLine();
